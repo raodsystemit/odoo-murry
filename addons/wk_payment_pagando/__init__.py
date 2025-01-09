fb610962e2ccaa176b0b2fba8a6540677ad4463d
rinkoo-webkul <rinkoo.kumar797@webkul.in>
2025-01-09 11:24:28 +0530

    [UPD] updated callback method in pagando payment

--- addons/wk_payment_pagando/__init__.py
+++ addons/wk_payment_pagando/__init__.py
@@ -10,16 +10,22 @@ from . import models
 
 from odoo.addons.payment import setup_provider, reset_payment_provider
 
+
 def pre_init_check(cr):
     from odoo.service import common
     from odoo.exceptions import UserError
+
     version_info = common.exp_version()
-    server_serie = version_info.get('server_serie')
-    if server_serie != '18.0':
-        raise UserError('Module support Odoo series 18.0 found {}.'.format(server_serie))
+    server_serie = version_info.get("server_serie")
+    if server_serie != "18.0":
+        raise UserError(
+            "Module support Odoo series 18.0 found {}.".format(server_serie)
+        )
+
 
 def post_init_hook(env):
-    setup_provider(env, 'pagando')
+    setup_provider(env, "pagando")
+
 
 def uninstall_hook(env):
-    reset_payment_provider(env, 'pagando')
+    reset_payment_provider(env, "pagando")
